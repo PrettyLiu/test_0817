19c2737dcbed9a94024c7ecdedc68df8f680fcd6
PrettyLiu <30800948+PrettyLiu@users.noreply.github.com>
2017-08-17 21:32:25 +0800

    Update a.java

--- a.java
+++ a.java
@@ -1,13 +1,13 @@
 a.java
 fffffflyo9;p80fffffytjr8ylofffff,kiutl9yfffff
 trur7ti
-<<<<<<< HEAD
+
 lllllllllllllll
 lllllllllllllllllllll
 lllllllllllllllllllll
-=======
+
 aaaaaaaaaaaaaaa
 aaaaaaaaaaaaaaaa
 aaaaaaaaaaaaaaaa
 dddddddddddddddddddddddddddddddddddddd
->>>>>>> 9831b84d108d766a271d48df42c8941a24846ec7
+
